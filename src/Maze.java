c437dc81bab6432b3a31c44720e768205a2a1048
gasiek <kacper.b.gasior@gmail.com>
2021-04-12 20:51:18 +0200

    Maze generation

---
+++ src/Maze.java
@@ -0,0 +1,62 @@
+
+public class Maze {
+    public int Rows = 60;
+    public int Columns = 80;
+    char[][] A = new char[Rows][Columns];
+    public final char OBSTACLE = '*';
+    public final char INITIAL_STATE = 'I';
+    public final char GOAL_STATE = 'G';
+    public final char BELONG = '+';
+    public final char WALKABLE = ' ';
+    public static double fraction = 0.3;
+    public int row;
+    public int column;
+
+    public Maze() {
+        this(fraction);
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Columns; j++) {
+                if (Math.random() < fraction) A[i][j] = OBSTACLE;
+                else A[i][j] = WALKABLE;
+            }
+        }
+        setState(INITIAL_STATE);
+        setState(GOAL_STATE);
+    }
+
+    public Maze(double newFraction) {
+        if(newFraction>0.9 || newFraction<0) {
+            System.out.println("The fraction has to be a value between 0 and 0.9. It has been changed to default value: " + fraction);
+            newFraction=fraction;
+        }
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Columns; j++) {
+                if (Math.random() < newFraction) A[i][j] = OBSTACLE;
+                else A[i][j] = WALKABLE;
+            }
+        }
+        setState(INITIAL_STATE);
+        setState(GOAL_STATE);
+    }
+
+    private void setState(char newState) {
+        do {
+            row = (int) (Math.random() * Rows);
+            column = (int) (Math.random() * Columns);
+        } while (A[row][column] != WALKABLE);
+        A[row][column] = newState;
+    }
+
+    private void notFound() {
+        System.out.println("Lectura de elementos de la matriz: ");
+    }
+
+    public void printMaze() {
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Columns; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args){
+        Maze testMaze = new Maze();
+        testMaze.printMaze();
+    }
+}
